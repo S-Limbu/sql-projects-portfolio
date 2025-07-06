7ff57388fa6ecfa31d00d8f4f034cc4f92e19809
Samuel Limbu <Samuel_Limbu@hotmail.com>
2025-07-06 23:30:00 +0100

    03-sales-by-region.sql

--- practice/03-sales-by-region.sql
+++ practice/03-sales-by-region.sql
@@ -1,8 +1,13 @@
 -- 🧠 Problem / Business Scenario: The Regional Sales Director wants you to analyze total revenue performance by region 
 to inform where it will be best to allocate additional marketing budget for the next quarter.
+  
 -- 📚 Concepts: GROUP BY + ORDER BY
--- 💡 Why this matters: Regional breakdowns help evaluate area-specific performance.
-
+  
+-- 💡 Why this matters: Regional breakdowns helps evaluate area-specific performance:
+  - Identify strong-performing regions for potential expansion.
+  - Detect any underperforming areas that may need support.
+  - Guide budget allocation, staffing, and promotions more effectively.
+  
 -- 🔄 1. Drop existing table to avoid conflicts
 
 DROP TABLE IF EXISTS orders;
