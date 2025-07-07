a7e6dfa9537616d0b3b660f96709196c526507b1
Samuel Limbu <Samuel_Limbu@hotmail.com>
2025-07-08 00:20:08 +0100

    11-flag-high-value-orders.sql

--- practice/11-flag-high-value-orders.sql
+++ practice/11-flag-high-value-orders.sql
@@ -1,7 +1,13 @@
--- 🧠 Problem: 
--- 📚 Concepts: CASE + ELSE
--- 💡 Why this matters: 
+-- 🧠 Problem / Business Scenario: The Finance and Sales Team wants to classify each order as either "High" or "Normal" 
+based on the amount of purchases. Orders over £500 are considered high-value and may require special handling or a follow-up.
 
+-- 📚 Concepts: CASE + ELSE
+
+-- 💡 Why this matters: 
+  - Segment orders for priority shipping or fraud checks.
+  - Target high-value customers with loyalty programs.
+  - Simplify dashboards by grouping order tiers.
+  
 -- 🔄 1. Drop existing table to avoid conflicts
 
 DROP TABLE IF EXISTS logins;
@@ -17,13 +23,15 @@ CREATE TABLE orders (
 -- 📊 3. Insert sample/mock data
 
 INSERT INTO orders VALUES
-(1, 1, 450), (2, 2, 600), (3, 3, 800);
+(1, 1, 450), (2, 2, 600), (3, 3, 800), ( 4, 4, 200), ( 5, 5, 450), 
+( 6, 6, 780), ( 7, 7, 650), ( 8, 8, 100), ( 9, 9, 100), ( 10, 10, 770 );
 
 -- 🔍 4. Final query
 
 SELECT *,
-       CASE WHEN amount > 500 THEN 'High' ELSE 'Normal' END AS value_flag
+CASE WHEN amount > 500 THEN 'High' ELSE 'Normal' END AS value_flag
 FROM orders;
 
 -- 🖼️ Screenshot: screenshots/11-flag-value-orders-result.png
--- 📝 Output: 
+-- 📝 Output: Seperates the value of orders to see which customers are spending more amounts of money for varius future
+programs.
