072ea97f43e6139931df89c8a0c1e8e1cad754e6
Samuel Limbu <Samuel_Limbu@hotmail.com>
2025-07-09 00:08:46 +0100

    12-customer-lifetime-spend.sql

--- practice/12-customer-lifetime-spend.sql
+++ practice/12-customer-lifetime-spend.sql
@@ -1,7 +1,14 @@
--- 🧠 Problem: 
--- 📚 Concepts: SUM + GROUP BY
--- 💡 Why this matters: 
-
+- 🧠 Problem / Business Scenario: The Marketing Team wants to 
+  understand how much each customer has spent in total since they first started purchasing. This metric is known as 
+  Customer Lifetime Value and is key to customer segmentation and retention strategies.
+  
+- 📚 Concepts: SUM + GROUP BY
+  
+- 💡 Why this matters: This matters for numerous reasons as;
+ - High-spending customers can receive exclusive deals and loyalty programs.
+ - Identifying top spenders can help priotize customer retention to those that counts the most.
+ - Grouping customers by total spend allows teams to tailor messaging and promotions effectively.
+   
 -- 🔄 1. Drop existing table to avoid conflicts
 
 DROP TABLE IF EXISTS orders;
@@ -22,7 +29,8 @@ INSERT INTO orders VALUES
 
 SELECT customer_id, SUM(amount) AS lifetime_spend
 FROM orders
-GROUP BY customer_id;
+GROUP BY customer_id
+ORDER BY lifetime_spend DESC;
 
--- 🖼️ Screenshot: screenshots/.png
--- 📝 Output: 
+-- 🖼️ Screenshot: screenshots/12-customer-lifetime-spend-result.png
+-- 📝 Output: Showcases the most loyal customer that has spent the most all time in a list.
