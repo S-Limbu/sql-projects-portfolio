70beb72652f9a176d466587009f4f0e86af0474e
Samuel Limbu <Samuel_Limbu@hotmail.com>
2025-07-10 00:38:13 +0100

    19-running-total-moving-avg.sql

--- practice/19-running-total-moving-avg.sql
+++ practice/19-running-total-moving-avg.sql
@@ -1,10 +1,17 @@
--- 🧠 Problem: 
--- 📚 Concepts: SUM() OVER + AVG() OVER + ROUND + ROWS BETWEEN + CURRENT ROW + ORDER BY
--- 💡 Why this matters: 
+- 🧠 Problem / Business Scenario: You’re a Financial Analyst working at a tech company. Your manager wants to analyze 
+daily cash flow and understand the cumulative revenue up to each date, alongside the 3-day moving average of transaction
+amounts to identify any trends.
+  
+- 📚 Concepts: SUM() OVER + AVG() OVER + ROUND + ROWS BETWEEN + CURRENT ROW + ORDER BY
+  
+- 💡 Why this matters: 
+  - Understand how revenue is building over time.
+  - Quickly spot days that deviate sharply from recent performance.
+  - Basis for trend forecasting and sales pacing for stakeholders.
 
 -- 🔄 1. Drop existing table to avoid conflicts
 
-DROP TABLE IF EXISTS funnel;
+DROP TABLE IF EXISTS transactions;
 
 -- 🧱 2. Create the table schema
 
@@ -21,7 +28,16 @@ INSERT INTO transactions VALUES
 (2, '2024-01-02', 200),
 (3, '2024-01-03', 150),
 (4, '2024-01-04', 180),
-(5, '2024-01-05', 300);
+(5, '2024-01-05', 300),
+(6, '2024-01-06', 490),
+(7, '2024-01-07', 500),
+(8, '2024-01-08', 300),
+(9, '2024-01-09', 200),
+(10, '2024-01-10', 210),
+(11, '2024-01-11', 320),
+(12, '2024-01-12', 340),
+(13, '2024-01-13', 400),
+(14, '2024-01-14', 450);
 
 -- 🔍 4. Final query
 
@@ -33,4 +49,6 @@ ROUND(AVG(amount) OVER (ORDER BY transaction_date ROWS BETWEEN 2 PRECEDING AND C
 FROM transactions;
 
 -- 🖼️ Screenshot: screenshots/19-running-total-moving-avg-result.png
--- 📝 Output: 
+-- 📝 Output: Shows the cumulative revenue overtime. An example of what’s expected in financial reporting and BI dashboards.
+
+
